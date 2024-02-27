c2be47e793c31cad10f9d2c4aa7d697742cc8729
Wilsonabdiel <wilsonabdiel86@gmail.com>
2024-02-27 17:15:38 +0100

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import '../styles/App.css';
+import Home from './Home.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/Home.css'
+
+const Home = () => (
+    <section>
+        <div>
+            <h1>Abdiel JW</h1>
+        </div>
+        <div>
+            <p>Hey humans, I am Abdiel, from the planet of open sourcerors.</p>
+        </div>
+        <div>
+            <p>Working at: </p>
+            <p>Creator of:</p>
+            <p>Team member at GDSC UNIUYO DEV TEAM.</p>
+        </div>
+        <div>
+            <p>I enjoy building tech I use, even if I get to use them just once. Check out my full project list here. I am a backend developer.</p>
+        </div>
+        <div>
+            <p>
+            Outside of programming, I have a passion for poetry. Some of my poems have been published. You can find them on [this site](ADD LINK TO SITES). Additionally, I'm an avid gamer. It would be great to connect and chat about our interests. Feel free to [contact me](LINK TO CONTACT).
+
+</p>
+        </div>
+    </section>
+         );
+
+export default Home;
